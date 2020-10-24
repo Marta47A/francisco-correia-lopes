808b5eec9bf2dc6f776bfebd85d0c73a77595ac4
Marta47A <marta.aparicio47@gmail.com>
2020-10-24 01:02:12 +0100

    Google authentication I.

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 const app = express();
 const _ = require('lodash');
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const multer = require('multer');
 
@@ -24,11 +29,119 @@ app.use(multer({
   },
 }));
 
+app.use(session({
+  secret: "This is a secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/francisco-correia-lopes_DB", {
   useNewUrlParser: true
 });
+mongoose.set("useCreateIndex", true);
 
 
+//Users
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String,
+  googleId: String,
+  secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/francisco-correia-lopes",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get("/auth/google/francisco-correia-lopes",
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/");
+  });
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", function(req, res){
+
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+
+});
+
+app.post("/login", function(req, res){
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+
+});
 
 
 //Posts
@@ -47,6 +160,8 @@ const postSchema = {
 
 const Post = mongoose.model("Post", postSchema);
 
+
+
 app.route("/posts")
 
   .get(function(req, res) {
@@ -58,6 +173,7 @@ app.route("/posts")
       }
     });
   })
+
 
   .post(function(req, res) {
 
@@ -72,6 +188,25 @@ app.route("/posts")
       img.data = fs.readFileSync(req.body.postImage),
       img.contentType = 'image / png'
     });
+
+
+
+
+    User.findById(req.user.id, function(err, foundUser){
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          foundUser.secret = submittedSecret;
+          foundUser.save(function(){
+            res.redirect("/secrets");
+          });
+        }
+      }
+    });
+
+
+
 
     post.save(function(err) {
       if (!err) {
@@ -324,12 +459,36 @@ app.get("/", function(req, res) {
   });
 });
 
-app.get("/compose", function(req, res) {
-  res.render("compose");
+app.get("/add-post", function(req, res) {
+  if (req.isAuthenticated()){
+    res.render("add-post");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.get("/add-photo", function(req, res) {
-  res.render("add-photo");
+  if (req.isAuthenticated()){
+    res.render("add-photo");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.delete("/posts/:postTitle/delete", function(req, res) {
+  if (req.isAuthenticated()){
+    res.render("posts");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.delete("/photos/:photoTitle/delete", function(req, res) {
+  if (req.isAuthenticated()){
+    res.render("submit");
+  } else {
+    res.redirect("/");
+  }
 });
 
 
